03386717c4751c259e3b59486ad32151d5b90166
Sammy Mustafa <sammymustafa@gmail.com>
2024-04-22 18:32:54 -0400

    embededded video demo

--- src/navigation.js
+++ src/navigation.js
@@ -11,7 +11,7 @@ export const headerData = {
       href: getPermalink('/#stakeholders'),
     },
     {
-      text: 'Walkthrough',
+      text: 'Walkthrough/Demo',
       href: getPermalink('/#walkthrough'),
     },
     {
@@ -22,8 +22,7 @@ export const headerData = {
       text: 'FAQs',
       href: getPermalink('/#faqs'),
     },
-  ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  ]
 };
 
 export const footerData = {
@@ -35,9 +34,5 @@ export const footerData = {
   socialLinks: [
     { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/bond-ehr' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/sammymustafa/bond-prototype' },
-  ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
+  ]
 };
