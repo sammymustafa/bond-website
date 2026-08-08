38059ca84906b9ca187356373822d341596123fe
Steven Tan <stevenheintan@gmail.com>
2026-08-07 17:37:56 -0700

    Rb2b script addition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -290,6 +290,11 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(key){if(window.reb2b)return;window.reb2b={loaded:true};var s=document.createElement("script");s.async=true;s.src="https://ddwl4m2hdecbv.cloudfront.net/b/"+key+"/"+key+".js.gz";document.getElementsByTagName("script")[0].parentNode.insertBefore(s,document.getElementsByTagName("script")[0]);}("QO92DHZ7JPN7");`,
+          }}
+        />
       </head>
       <body className="antialiased">
         <ClientWrapper>
